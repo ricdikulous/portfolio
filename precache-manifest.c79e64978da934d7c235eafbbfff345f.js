10a8e2c653eaf921c9c8ca0927f8730176c06246
Richard Watts-Seale <rwattsseale@gmail.com>
2022-05-30 10:00:39 +0200

    Updates

---
+++ precache-manifest.c79e64978da934d7c235eafbbfff345f.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed0971ec02aa18a2c49bab6b5c12e5e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4eb87ff6f1fb39432bd9",
+    "url": "/portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "c94662e04e2fc10346da",
+    "url": "/portfolio/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "c03cc253c38f0878c3f5",
+    "url": "/portfolio/static/css/main.9a9072c2.chunk.css"
+  },
+  {
+    "revision": "fd9e4d526083cf15be95",
+    "url": "/portfolio/static/js/2.7c6d5f3c.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/portfolio/static/js/2.7c6d5f3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb87ff6f1fb39432bd9",
+    "url": "/portfolio/static/js/3.625aa6e9.chunk.js"
+  },
+  {
+    "revision": "c94662e04e2fc10346da",
+    "url": "/portfolio/static/js/4.011c95b8.chunk.js"
+  },
+  {
+    "revision": "c03cc253c38f0878c3f5",
+    "url": "/portfolio/static/js/main.5f3c1e95.chunk.js"
+  },
+  {
+    "revision": "f75e0130e5cd0cce8458",
+    "url": "/portfolio/static/js/runtime-main.9d895020.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/portfolio/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "bd0e5381e53f21b806a147032679c9e8",
+    "url": "/portfolio/static/media/pageApproval.bd0e5381.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "0d5a8b353b86a511ed1c1140136201f7",
+    "url": "/portfolio/static/media/reporting.0d5a8b35.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "40963b27008b1d4273572c3f1a411bf7",
+    "url": "/portfolio/static/media/skillpacaLogo.40963b27.png"
+  },
+  {
+    "revision": "3e02f599c42d65105008da86aa944d02",
+    "url": "/portfolio/static/media/skillpacaTransparent.3e02f599.png"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/portfolio/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
